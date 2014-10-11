47c00f718760be780f212e86c1b9a66f2915d08b
darkunor <k983533@yahoo.co.uk>
2014-10-11 12:46:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameSetAndChat::Application.config.secret_token = '790bee2704aa3032beccdc9afbbaf2df1bfb92d9b15d13acad5912f1cd1f59cfc8163e7844ecd99e6b8c6a92599ed794d0df2913afbbe048fdff439888cda751'
