a5baa3d43a498bd66f5b6429fec392c17bbfa1a3
darkunor <k983533@yahoo.co.uk>
2014-10-22 12:02:32 +0100

    made topic of the day link to topic in desired forum

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ GameSetAndChat::Application.routes.draw do
 
   devise_for :users
   
+  # devise_scope :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
+  #   post '/users/sign_in' => 'devise/sessions#create', :as => :user_session
+  # end
+
   root to: "forums#index"
 
   resources :forums
   resources :users
   resources :topics
   resources :posts
-
+  resources :about
+  resources :house
 end
